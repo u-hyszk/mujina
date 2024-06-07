d9e2ca396a15c9119a44e755f47c6a5c2d54924c
u-hyszk <hayashizaki.yu.t5@dc.tohoku.ac.jp>
2024-06-07 13:46:35 +0900

    dev: PDFのIOの開発途中での一時保存

--- src/mujina/lambda_function.py
+++ src/mujina/lambda_function.py
@@ -5,9 +5,18 @@ import re
 from io import BytesIO
 
 import requests
+from pypdf import PdfReader
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
+
+
+# 環境変数
+SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
+SLACK_APP_TOKEN = os.environ.get("SLACK_APP_TOKEN")
+
+# APIのURL
+SLACK_API_URL = "https://slack.com/api/"
 
 
 def respond(res=None):
@@ -23,10 +32,10 @@ def respond(res=None):
 
 
 def post_message_to_slack(channel, message):
-    url = "https://slack.com/api/chat.postMessage"
+    url = os.path.join(SLACK_API_URL, "chat.postMessage")
     headers = {
         "Content-Type": "application/json; charset=UTF-8",
-        "Authorization": f"Bearer {os.environ.get('SLACK_BOT_TOKEN')}",
+        "Authorization": f"Bearer {SLACK_BOT_TOKEN}",
     }
     data = {
         "text": message,
@@ -41,7 +50,7 @@ def post_message_to_slack(channel, message):
 
 def download_pdf(file_url):
     headers = {
-        "Authorization": f"Bearer {os.environ.get('SLACK_BOT_TOKEN')}",
+        "Authorization": f"Bearer {SLACK_BOT_TOKEN}",
     }
     res = requests.get(file_url, headers=headers)
     res.raise_for_status()
@@ -49,14 +58,33 @@ def download_pdf(file_url):
     return pdf_stream
 
 
-def get_message_from_slack(event):
+def extract_text_from_pdf(pdf_stream):
+    reader = PdfReader(pdf_stream)
+    n_pages = len(reader.pages)
+    texts = []
+    for page_idx in range(n_pages):
+        page = reader.pages[page_idx]
+        page_text = page.extract_text()
+        texts.append(page_text)
+    return "".join(texts)
+
+
+def extract_message_from_slack(event):
     text = event["event"]["text"]  # 投稿されたテキスト全体を取得
     message = re.sub(r"<@.*?>", "", text)  # メンションを削除
     return message
 
 
-def get_channel_from_slack(event):
+def extract_channel_from_slack(event):
     return event["event"]["channel"]
+
+
+def extract_pdf_url_from_slack(event):
+    if "files" in event["event"]:
+        for file_info in event["event"]["files"]:
+            if file_info["filetype"] == "pdf":
+                return file_info["url_private"]
+    return None
 
 
 def lambda_handler(event, context):
@@ -69,8 +97,13 @@ def lambda_handler(event, context):
 
     try:
         # オウム返し
-        message = get_message_from_slack(event)
-        channel = get_channel_from_slack(event)
+        message = extract_message_from_slack(event)
+        channel = extract_channel_from_slack(event)
+        pdf_url = extract_pdf_url_from_slack(event)
+        if pdf_url:
+            pdf_stream = download_pdf(pdf_url)
+            text = extract_text_from_pdf(pdf_stream)
+            post_message_to_slack(channel, text)
         post_message_to_slack(channel, message)
     except Exception as e:
         LOGGER.error(f"Error: {e}")
